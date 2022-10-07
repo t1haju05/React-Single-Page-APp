6cd1971fba2993980e4930be31d17541fc67b8e4
t1haju05 <t1haju05@students.oamk.fi>
2022-10-07 11:20:08 +0300

    Edittiä

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ function NavBar() {
     return (
         <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">MR. Green</a>
+                <a class="navbar-brand" href="/">MR. Green</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
